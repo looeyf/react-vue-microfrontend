8b4007aad419350d6fd0d597c699cd2aaa2852b7
Luis Oliveira <luis10.pf@gmail.com>
2024-09-21 19:49:35 -0300

    setup react for marketing project

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (element) => ReactDOM.render(<h1>Hello!</h1>, element);
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) mount(devRoot);
+}
+
+export { mount };
