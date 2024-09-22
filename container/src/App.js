965d605b3a984b68a4c815528555421283983436
Luis Oliveira <luis10.pf@gmail.com>
2024-09-21 21:25:02 -0300

    integrate host project with marketing project

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <h1>Container!</h1>
+  return (
+    <>
+      <h1>Container</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  )
 };
